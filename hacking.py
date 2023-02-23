1327b6c9dae1ac8e260d4e0b2174a7ac674e5ba6
Rafael Dantas <89138596+Danzokka@users.noreply.github.com>
2023-02-22 21:25:46 -0300

    Add files via upload

--- hacking.py
+++ hacking.py
@@ -5,135 +5,135 @@ import sys
 
 GARBAGE_CHARS = '~!@#$%^&*()_+-={}[]|;:,.<>?/'
 
-with open('sevenletterwords.txt') as wordListFile:
-    WORDS = wordListFile.readlines()
-    for i in range(len(WORDS)):
-        WORDS[i] = WORDS[i].strip().upper()
+with open('palavras-sete-letras.txt') as arquivoListaPalavras:
+    PALAVRAS = arquivoListaPalavras.readlines()
+    for i in range(len(PALAVRAS)):
+        PALAVRAS[i] = PALAVRAS[i].strip().upper()
 
 
 def main():
     input('Pressione Enter para iniciar...')
 
-    gameWords = getWords()
-    computerMemory = getComputerMemoryString(gameWords)
-    secretPassword = random.choice(gameWords)
+    gamePalavras = getPalavras()
+    memoriaComputador = getMemoriaComputadorString(gamePalavras)
+    senhaSecreta = random.choice(gamePalavras)
 
-    print(computerMemory)
-    for triesRemaining in range(4, 0, -1):
-        playerMove = askForPlayerGuess(gameWords, triesRemaining)
-        if playerMove == secretPassword:
+    print(memoriaComputador)
+    for tentativasRestantes in range(4, 0, -1):
+        movimentoJogador = pergunteChuteJogador(gamePalavras, tentativasRestantes)
+        if movimentoJogador == senhaSecreta:
             print('A C C E S S O  G A R A N T I D O')
             return
         else:
-            numMatches = numMatchingLetters(secretPassword, playerMove)
-            print('Accesso Negado ({}/7 corretas)'.format(numMatches))
-    print('Sem tentativas. A senha era {}.'.format(secretPassword))
+            numeroLetrasCorretas = numeroLetrasRelacionadas(senhaSecreta, movimentoJogador)
+            print('Accesso Negado ({}/7 corretas)'.format(numeroLetrasCorretas))
+    print('Sem tentativas. A senha era {}.'.format(senhaSecreta))
 
 
-def getWords():
+def getPalavras():
 
-    secretPassword = random.choice(WORDS)
-    words = [secretPassword]
+    senhaSecreta = random.choice(PALAVRAS)
+    palavras = [senhaSecreta]
 
-    while len(words) < 3:
-        randomWord = getOneWordExcept(words)
-        if numMatchingLetters(secretPassword, randomWord) == 0:
-            words.append(randomWord) #acha palavras que não tem letras correspondentes a palavra secreta
-
-    for i in range(500):
-        if len(words) == 5:
-            break  # Found 5 words, so break out of the loop.
-
-        randomWord = getOneWordExcept(words)
-        if numMatchingLetters(secretPassword, randomWord) == 3:
-            words.append(randomWord) #acha uma palavra com 3 letras e adiciona na matriz de palavras
+    while len(palavras) < 3:
+        palavraAleatoria = getUmaPalavraExceto(palavras)
+        if numeroLetrasRelacionadas(senhaSecreta, palavraAleatoria) == 0:
+            palavras.append(palavraAleatoria) #acha palavras que não tem letras correspondentes a palavra secreta
 
     for i in range(500):
-        if len(words) == 12:
+        if len(palavras) == 5:
+            break  # Found 5 palavras, so break out of the loop.
+
+        palavraAleatoria = getUmaPalavraExceto(palavras)
+        if numeroLetrasRelacionadas(senhaSecreta, palavraAleatoria) == 3:
+            palavras.append(palavraAleatoria) #acha uma palavra com 3 letras e adiciona na matriz de palavras
+
+    for i in range(500):
+        if len(palavras) == 12:
             break  # Quando achar 7 palavras quebra o loop
 
-        randomWord = getOneWordExcept(words)
-        if numMatchingLetters(secretPassword, randomWord) != 0:
-            words.append(randomWord) # adiciona palavras que tem pelo menos uma letra igual a palavra secreta
+        palavraAleatoria = getUmaPalavraExceto(palavras)
+        if numeroLetrasRelacionadas(senhaSecreta, palavraAleatoria) != 0:
+            palavras.append(palavraAleatoria) # adiciona palavras que tem pelo menos uma letra igual a palavra secreta
 
-        while len(words) < 12:
-            randomWord = getOneWordExcept(words)
-            words.append(randomWord) #adiciona palavras para fechar a matriz
+        while len(palavras) < 12:
+            palavraAleatoria = getUmaPalavraExceto(palavras)
+            palavras.append(palavraAleatoria) #adiciona palavras para fechar a matriz
 
-    assert len(words) == 12
-    return words
+    assert len(palavras) == 12
+    return palavras
 
 
-def getOneWordExcept(blocklist=None):
+def getUmaPalavraExceto(blocklist=None):
     if blocklist == None:
         blocklist = []
 
     while True:
-        randomWord = random.choice(WORDS)
-        if randomWord not in blocklist:
-            return randomWord
+        palavraAleatoria = random.choice(PALAVRAS)
+        if palavraAleatoria not in blocklist:
+            return palavraAleatoria
 
 
-def numMatchingLetters(word1, word2):
-    matches = 0
+def numeroLetrasRelacionadas(word1, word2):
+    ocorrencias = 0
     for i in range(len(word1)):
         if word1[i] == word2[i]:
-            matches += 1
-    return matches
+            ocorrencias += 1
+    return ocorrencias
 
 
-def getComputerMemoryString(words):
+def getMemoriaComputadorString(palavras):
 
     # Divide em 2 partes que tem 16 caracteres
-    linesWithWords = random.sample(range(16 * 2), len(words))
+    linhasComPalavras = random.sample(range(16 * 2), len(palavras))
     # A memoria que ele representa
-    memoryAddress = 16 * random.randint(0, 4000)
+    enderecoMemoria = 16 * random.randint(0, 4000)
 
     # Cria a "memoria" do computador
-    computerMemory = []  # Contem 16 strings para cada lado
-    nextWord = 0  # O indexador
-    for lineNum in range(16):
+    memoriaComputador = []  # Contem 16 strings para cada lado
+    proximaPalavra = 0  # O indexador
+    for numeroLinha in range(16):
         # Cria metade da linha como caracteres de lixo
-        leftHalf = ''
-        rightHalf = ''
+        parteEsquerda = ''
+        parteDireita = ''
         for j in range(16):
-            leftHalf += random.choice(GARBAGE_CHARS)
-            rightHalf += random.choice(GARBAGE_CHARS)
+            parteEsquerda += random.choice(GARBAGE_CHARS)
+            parteDireita += random.choice(GARBAGE_CHARS)
 
-        if lineNum in linesWithWords:
+        if numeroLinha in linhasComPalavras:
             # Acha um lugar aleatório para colocar as palavras:
-            insertionIndex = random.randint(0, 9)
+            indexInseracao = random.randint(0, 9)
             # Insere a palavra:
-            leftHalf = (leftHalf[:insertionIndex] + words[nextWord]
-                + leftHalf[insertionIndex + 7:])
-            nextWord += 1
-        if lineNum + 16 in linesWithWords:
+            parteEsquerda = (parteEsquerda[:indexInseracao] + palavras[proximaPalavra]
+                + parteEsquerda[indexInseracao + 7:])
+            proximaPalavra += 1
+        if numeroLinha + 16 in linhasComPalavras:
             # Acha um lugar aleatório para colocar as palavras na do 2º lado:
-            insertionIndex = random.randint(0, 9)
+            indexInseracao = random.randint(0, 9)
             # Insere a palavra:
-            rightHalf = (rightHalf[:insertionIndex] + words[nextWord]
-                + rightHalf[insertionIndex + 7:])
-            nextWord += 1
+            parteDireita = (parteDireita[:indexInseracao] + palavras[proximaPalavra]
+                + parteDireita[indexInseracao + 7:])
+            proximaPalavra += 1
 
-        computerMemory.append('0x' + hex(memoryAddress)[2:].zfill(4)
-                    + '  ' + leftHalf + '    '
-                    + '0x' + hex(memoryAddress + (16*16))[2:].zfill(4)
-                    + '  ' + rightHalf)
+        memoriaComputador.append('0x' + hex(enderecoMemoria)[2:].zfill(4)
+                    + '  ' + parteEsquerda + '    '
+                    + '0x' + hex(enderecoMemoria + (16*16))[2:].zfill(4)
+                    + '  ' + parteDireita)
 
-        memoryAddress += 16  # Jump from, say, 0xe680 to 0xe690.
+        enderecoMemoria += 16  # Jump from, say, 0xe680 to 0xe690.
 
     # Cada string na matriz de memoria do computador é juntada em uma só
-    return '\n'.join(computerMemory)
+    return '\n'.join(memoriaComputador)
 
 
-def askForPlayerGuess(words, tries):
+def pergunteChuteJogador(palavras, tentativas):
     while True:
-        print('Insira a senha: ({} tentativas restantes)'.format(tries))
-        guess = input('> ').upper()
-        if guess in words:
-            return guess
+        print('Insira a senha: ({} tentativas restantes)'.format(tentativas))
+        chute = input('> ').upper()
+        if chute in palavras:
+            return chute
         print('Essa palavra não consta nas palavras listadas.')
-        print('Tente inserir "{}" ou "{}".'.format(words[0], words[1]))
+        print('Tente inserir "{}" ou "{}".'.format(palavras[0], palavras[1]))
 
 
 if __name__ == '__main__':
